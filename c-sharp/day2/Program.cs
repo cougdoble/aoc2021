c5597e293213d410f3ec70f4e6caab398a6eb38a
Douglas Coble <dcoble@relias.com>
2021-12-02 22:56:04 -0500

    minor cleanup

--- c-sharp/day2/Program.cs
+++ c-sharp/day2/Program.cs
@@ -1,74 +1,76 @@
 ﻿// not proud of this, but it is what it is :(
 static int CalcDepthAndPosition(string filePath)
 {
-    var input = new List<string>();
-    using (var streamReader = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
-    {
-        string? line;
-        while ((line = streamReader.ReadLine()) is not null)
-        {
-            input.Add(line);
-        }
-    }
+	var input = new List<string>();
+	using (var streamReader = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+	{
+		string? line;
+		while ((line = streamReader.ReadLine()) is not null)
+		{
+			input.Add(line);
+		}
+	}
 
-    var horizontalPosition = 0;
-    var depth = 0;
+	var horizontalPosition = 0;
+	var depth = 0;
 
-    foreach (var movement in input.Select(i => i.Split(" ")))
-    {
-        switch (movement[0])
-        {
-            case "forward":
-                horizontalPosition += int.Parse(movement[1]);
-                break;
-            case "down":
-                depth += int.Parse(movement[1]);
-                break;
-            case "up":
-                depth -= int.Parse(movement[1]);
-                break;
-        }
-    }
+	foreach (var movement in input.Select(i => i.Split(" ")))
+	{
+		var amount = int.Parse(movement[1]);
+		switch (movement[0])
+		{
+			case "forward":
+				horizontalPosition += amount;
+				break;
+			case "down":
+				depth += amount;
+				break;
+			case "up":
+				depth -= amount;
+				break;
+		}
+	}
 
-    return horizontalPosition * depth;
+	return horizontalPosition * depth;
 }
 
 static int CalcDepthAndPosition2(string filePath)
 {
-    var input = new List<string>();
-    using (var streamReader = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
-    {
-        string? line;
-        while ((line = streamReader.ReadLine()) is not null)
-        {
-            input.Add(line);
-        }
-    }
+	var input = new List<string>();
+	using (var streamReader = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+	{
+		string? line;
+		while ((line = streamReader.ReadLine()) is not null)
+		{
+			input.Add(line);
+		}
+	}
 
-    var horizontalPosition = 0;
-    var depth = 0;
-    var aim = 0;
+	var horizontalPosition = 0;
+	var depth = 0;
+	var aim = 0;
 
-    foreach (var movement in input.Select(i => i.Split(" ")))
-    {
-        switch (movement[0])
-        {
-            case "forward":
-                horizontalPosition += int.Parse(movement[1]);
-                depth += int.Parse(movement[1]) * aim;
-                break;
-            case "down":
-                aim += int.Parse(movement[1]);
-                break;
-            case "up":
-                aim -= int.Parse(movement[1]);
-                break;
-        }
-    }
+	foreach (var movement in input.Select(i => i.Split(" ")))
+	{
+		var amount = int.Parse(movement[1]);
+		switch (movement[0])
+		{
+			case "forward":
+				horizontalPosition += amount;
+				depth += amount * aim;
+				break;
+			case "down":
+				aim += amount;
+				break;
+			case "up":
+				aim -= amount;
+				break;
+		}
+	}
 
-    Console.WriteLine($"horizontal position: {horizontalPosition}");
-    Console.WriteLine($"depth: {depth}");
-    return horizontalPosition * depth;
+	Console.WriteLine($"horizontal position: {horizontalPosition}");
+	Console.WriteLine($"depth: {depth}");
+	t return horizontalPosition * depth;
 }
 
 const string testFilePath = @"day2-test-input.txt";
